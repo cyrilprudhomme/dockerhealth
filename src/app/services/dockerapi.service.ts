2af7b256802a6bdc743d813811025ee328acc51e
SIU\extcprudhomme <cyril.prudhomme.ext@cibtp.fr>
2023-01-30 15:01:15 +0100

    [FEAT]-Ajout du module PWA

--- src/app/services/dockerapi.service.ts
+++ src/app/services/dockerapi.service.ts
@@ -10,20 +10,6 @@ export class DockerapiService {
   }
 
   getAllContainers() {
-    'use strict';
-    const {Docker} = require('node-docker-api');
-
-    const docker = new Docker({socketPath: '/var/run/docker.sock'});
-
-// List
-    docker.container.list()
-      // Inspect
-      .then((containers: { status: () => any; }[]) => containers[0].status())
-      .then((container: { stats: () => any; }) => container.stats())
-      .then((stats: { on: (arg0: string, arg1: { (stat: any): void; (err: any): void; }) => void; }) => {
-        stats.on('data', stat => console.log('Stats: ', stat.toString()))
-        stats.on('error', err => console.log('Error: ', err))
-      })
-      .catch((error: any) => console.log(error));
+    return null
   }
 }
